d208f8fcc399b256acd3b2f7926fb1a02a3e2856
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-06-24 23:40:49 +0900

    auth header from client

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,27 @@ import {
 } from "@apollo/client";
 import React from "react";
 
-const link = createHttpLink({
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
   uri: "http://localhost:4000",
-  credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "Bearer nothingnothing",
+    },
+  };
 });
 
 const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  link,
 });
 
 const EXCHANGE_RATES = gql`
