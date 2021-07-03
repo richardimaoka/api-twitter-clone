5b5667952a7d1ef57692002f2a5ee65e00b06886
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-07-03 09:45:49 +0900

    an attempt to use max id but failing

--- server/index.ts
+++ server/index.ts
@@ -20,9 +20,10 @@ const resolvers = {
   },
   Mutation: {
     addTweet: async (parent, args, context, info) => {
+      console.log("maxTweetId = ", context.maxTweetId)
       const current = new Date()
       const tweet = {
-        id : 10,
+        id : context.maxTweetId,
         user: context.loginUser,
         createdAt: current.toISOString() ,
         fullText: args.fullText,
@@ -34,11 +35,10 @@ const resolvers = {
 
       try {
         const result = await axios.post('http://localhost:3001/timeline.tweets/', tweet)
-        console.log("returning returning returning")
         return tweet
       } catch (e) {
         console.log("Server error happend on mutatoin addTweet!!!")
-        console.log(e)
+        console.log(e.stack)
         throw e
       }
     }
@@ -53,9 +53,24 @@ const server = new ApolloServer({
   context: async ({req}) => {
     const authToken = req.headers.authorization || '';
     const user = getUser(authToken);
+    const tweets =  (await axios.get('http://localhost:3001/tweets'));
+   
+    const toIdNumber = number => {
+      const parsed = Number.parseInt(number)
+      console.log("parsed = ", parsed)
+      if(Number.isNaN(parsed)) return 0
+      else return parsed
+    }
+    const maxTweetId = tweets.data.reduce((x, y) => {
+      console.log("x = ", x)
+      console.log("y = ", y)
+      return Math.max(toIdNumber(x.id), toIdNumber(y.id))
+    });
+
     return ({
       loginUser: user,
-      tweets: await axios.get('http://localhost:3001/tweets'),
+      tweets: tweets,
+      maxTweetId: maxTweetId,
       timelineTweets: await axios.get('http://localhost:3001/timeline.tweets')
     })
   }
