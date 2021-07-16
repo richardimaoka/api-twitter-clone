d008cad5ac3e84f9a86210a89efd92202a276f2d
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-07-17 07:26:46 +0900

    flex profile bar

--- client/src/TweetProfilerBar.tsx
+++ client/src/TweetProfilerBar.tsx
@@ -1,9 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import { DisplayName } from "./DisplayName";
 import { TweetTime } from "./TweetTime";
 import { ScreenName } from "./ScreenName"
 
 export const TweetProfileBar = ({displayName, screenName, tweetTime}: TweetProfileBarProps) =>
-  <div>
+  <div
+    css={css`
+    display: flex;
+  `}>
     <DisplayName name={displayName} />
     <ScreenName name={screenName}/>
     <TweetTime time={tweetTime} />
