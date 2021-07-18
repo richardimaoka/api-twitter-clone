3da19ca1708e1067b187e2dc2ab80b9f44169f9a
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-07-18 15:04:10 +0900

    move Content to TweetList

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,12 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  useQuery,
-  useMutation,
-  gql,
   createHttpLink,
   from,
 } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 import { setContext } from "@apollo/client/link/context";
-import React from "react";
-import { useState } from "react";
-import { QueryData, Tweet } from "./Twitter";
 import { TweetList } from "./TweetList";
-import { User } from "./Twitter";
-import { AddTweetBox } from "./AddTweetBox";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
@@ -47,83 +39,23 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 const client = new ApolloClient({
   link: from([errorLink, authLink.concat(httpLink)]),
   cache: new InMemoryCache(),
+  credentials: "same-origin",
 });
 
-const QUERY = gql`
-  query {
-    timeline {
-      tweets {
-        id
-        createdAt
-        fullText
-        favoriteCount
-        retweetCount
-        replyCount
-        user {
-          screenName
-          profileImageUrl
-        }
-      }
-    }
-  }
-`;
-
-const MUTATION = gql`
-  mutation ($fullText: String!) {
-    addTweet(fullText: $fullText) {
-      id
-      createdAt
-      fullText
-      favoriteCount
-      replyCount
-      retweetCount
-      quoteCount
-    }
-  }
-`;
-
 const App = () => {
-  return (
-    <div>
-      <ApolloProvider client={client}>
-        <Content />
-      </ApolloProvider>
-    </div>
-  );
-};
-interface ContentState {
-  queryState: "success" | "error" | "loading";
-  error: Error | undefined;
-  tweets: Tweet[];
-}
-
-type AddTweet = (t: Tweet) => void;
-
-const Content = () => {
-  const { loading, error, data } = useQuery<QueryData>(QUERY);
-
   const user = {
     id: "fakeUserId",
     screenName: "fakeScreenName",
     url: "https://example.com",
     profileImageUrl: "https://example.com",
   };
-  if (loading) {
-    return <p>Loading ...</p>;
-  } else if (error) {
-    return <p>Remote Server Error :(</p>;
-  } else if (typeof data == "undefined") {
-    return <p>Remote Server Error :(</p>;
-  } else {
-    return (
-      <React.Fragment>
-        <AddTweetBox user={user} />
-        <header>
-          <TweetList tweets={data.timeline.tweets} />
-        </header>
-      </React.Fragment>
-    );
-  }
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <TweetList user={user} />;
+      </ApolloProvider>
+    </div>
+  );
 };
 
 export default App;
--- client/src/TweetList.tsx
+++ client/src/TweetList.tsx
@@ -1,26 +1,61 @@
-import { Tweet } from "./Twitter";
+import { User, QueryData } from "./Twitter";
 import { TweetBox } from "./TweetBox";
+import { useQuery, gql } from "@apollo/client";
+import { AddTweetBox } from "./AddTweetBox";
+import React from "react";
 
-export const TweetList = ({ tweets }: TweetListProps) => {
-  return (
-    <div>
-      {tweets.map((t) => (
-        <div key={t.id}>
-          <TweetBox
-            displayName={t.user.screenName}
-            screenName={t.user.screenName}
-            tweetTime={t.createdAt}
-            fullText={t.fullText}
-            replyCount={t.replyCount}
-            likeCount={t.favoriteCount}
-            retweetCount={t.retweetCount}
-          />
-        </div>
-      ))}
-    </div>
+export const TweetList = ({ user }: TweetListProps) => {
+  const { loading, error, data } = useQuery<QueryData>(
+    gql`
+      query {
+        timeline {
+          tweets {
+            id
+            createdAt
+            fullText
+            favoriteCount
+            retweetCount
+            replyCount
+            user {
+              screenName
+              profileImageUrl
+            }
+          }
+        }
+      }
+    `
   );
+
+  if (loading) {
+    return <p>Loading ...</p>;
+  } else if (error) {
+    return <p>Remote Server Error :(</p>;
+  } else if (typeof data == "undefined") {
+    return <p>Remote Server Error :(</p>;
+  } else {
+    return (
+      <React.Fragment>
+        <AddTweetBox user={user} />
+        <header>
+          {data.timeline.tweets.map((t) => (
+            <div key={t.id}>
+              <TweetBox
+                displayName={t.user.screenName}
+                screenName={t.user.screenName}
+                tweetTime={t.createdAt}
+                fullText={t.fullText}
+                replyCount={t.replyCount}
+                likeCount={t.favoriteCount}
+                retweetCount={t.retweetCount}
+              />
+            </div>
+          ))}
+        </header>
+      </React.Fragment>
+    );
+  }
 };
 
 interface TweetListProps {
-  tweets: Tweet[];
+  user: User;
 }
