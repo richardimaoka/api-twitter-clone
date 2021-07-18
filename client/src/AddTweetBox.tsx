06243dcd5f476cc91dbdc835c15e2c8c97e06bc9
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-07-18 13:55:16 +0900

    AddTweetBox

--- client/src/AddTweetBox.tsx
+++ client/src/AddTweetBox.tsx
@@ -2,39 +2,43 @@ import { User, Tweet } from "./Twitter";
 import { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
 
-const MUTATION = gql`
-  mutation ($fullText: String!) {
-    addTweet(fullText: $fullText) {
-      id
-      createdAt
-      fullText
-      favoriteCount
-      replyCount
-      retweetCount
-      quoteCount
-    }
-  }
-`;
-
 export const AddTweetBox = ({ user }: AddTweetBoxProps) => {
-  const [inputValue, setValue] = useState<string>("");
-  const [addTweetMutation, { loading: mutationLoading, error: mutationError }] =
-    useMutation<Tweet>(MUTATION, {
-      onCompleted: (tweet: Tweet) => prependTweetToModel(tweet),
-    });
+  const [inputFormTextValue, setInputFormTextValue] = useState<string>("");
+  const [triggerMutation, { loading: mutationLoading, error: mutationError }] =
+    useMutation<Tweet>(
+      gql`
+        # fullText supplied by GraphQL Variables
+        mutation ($fullText: String!) {
+          addTweet(fullText: $fullText) {
+            id
+            createdAt
+            fullText
+            favoriteCount
+            replyCount
+            retweetCount
+            quoteCount
+          }
+        }
+      `,
+      {
+        update: () => {}, // TODO: implement this
+      }
+    );
 
-  const addTweetCallback = (fullText: string): void => {
-    addTweetMutation({ variables: { fullText: fullText } });
+  const tweetButtonCallback = (inputTextValue: string): void => {
+    triggerMutation({ variables: { fullText: inputTextValue } });
   };
 
   return (
     <div>
       <input
         placeholder="いまどうしてる？"
-        value={inputValue}
-        onChange={(input) => setValue(input.target.value)}
+        value={inputFormTextValue}
+        onChange={(input) => setInputFormTextValue(input.target.value)}
       />
-      <button onClick={() => addTweetCallback(inputValue)}>ツイートする</button>
+      <button onClick={() => tweetButtonCallback(inputFormTextValue)}>
+        ツイートする
+      </button>
     </div>
   );
 };
