fbd63afad36c09c67a8f8087987b20b2e3c0455c
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-08-01 12:34:25 +0900

    profile

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 const { ApolloServer, gql } = require("apollo-server");
-const {GraphQLScalarType, Kind } = require('graphql');
+const { GraphQLScalarType, Kind } = require("graphql");
 const fs = require("fs");
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 const typeDefs = gql`
   ${fs.readFileSync(__dirname.concat("/twitter.gql"), "utf8")}
@@ -9,39 +9,39 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    tweets(parent, args, context, info) {
-      return context.tweets
-    },
-    timeline(parent, args, context, info) {
+    profile(parent, args, context, info) {
       return {
-        tweets: context.timelineTweets
-      }
-    }
+        tweets: context.profileTweets,
+      };
+    },
   },
   Mutation: {
     addTweet: async (parent, args, context, info) => {
-      const current = new Date()
+      const current = new Date();
       const tweet = {
-        id : context.maxTweetId + 1,
+        id: context.maxTweetId + 1,
         user: context.loginUser,
-        createdAt: current.toISOString() ,
+        createdAt: current.toISOString(),
         fullText: args.fullText,
         favoriteCount: 0,
         replyCount: 0,
         retweetCount: 0,
-        quoteCount: 0
-      }
+        quoteCount: 0,
+      };
 
       try {
-        const result = await axios.post('http://localhost:3001/timeline.tweets/', tweet)
-        return tweet
+        const result = await axios.post(
+          "http://localhost:3001/timeline.tweets/",
+          tweet
+        );
+        return tweet;
       } catch (e) {
-        console.log("Server error happend on mutatoin addTweet!!!")
-        console.log(e.stack)
-        throw e
+        console.log("Server error happend on mutatoin addTweet!!!");
+        console.log(e.stack);
+        throw e;
       }
-    }
-  }
+    },
+  },
 };
 
 const server = new ApolloServer({
@@ -49,34 +49,35 @@ const server = new ApolloServer({
   resolvers,
   mocks: true,
   mockEntireSchema: false,
-  context: async ({req}) => {
-    const authToken = req.headers.authorization || '';
+  context: async ({ req }) => {
+    const authToken = req.headers.authorization || "";
     const user = getUser(authToken);
-    const tweets =  (await axios.get('http://localhost:3001/tweets')).data.sort((x, y) => Date.parse(y.createdAt) - Date.parse(x.createdAt));
-    const timelineTweets =  (await axios.get('http://localhost:3001/timeline.tweets')).data.sort((x, y) => Date.parse(y.createdAt) - Date.parse(x.createdAt));
+    const profileTweets = (
+      await axios.get("http://localhost:3001/profile.tweets")
+    ).data.sort((x, y) => Date.parse(y.createdAt) - Date.parse(x.createdAt));
 
-    const maxTweetId = timelineTweets.map(tw => {
-       if(tw.id > Number.MAX_SAFE_INTEGER) return 0
-       else return tw.id 
-    }).reduce(
-      (accumulator, current) => Math.max(accumulator, current)
-    )
+    const maxTweetId = profileTweets
+      .map((tw) => {
+        if (tw.id > Number.MAX_SAFE_INTEGER) return 0;
+        else return tw.id;
+      })
+      .reduce((accumulator, current) => Math.max(accumulator, current));
 
-    return ({
+    return {
       loginUser: user,
-      tweets: tweets,
       maxTweetId: maxTweetId,
-      timelineTweets: timelineTweets
-    })
-  }
+      profileTweets: profileTweets,
+    };
+  },
 });
 
 const getUser = (authToken) => ({
   id: 2875908842,
   screenName: "RichardImaokaJP",
   url: "https://blog-jp.richardimaoka.net",
-  profileImageUrl: "http://pbs.twimg.com/profile_images/1208118895741001728/-t2DIJt-_normal.jpg"
-})
+  profileImageUrl:
+    "http://pbs.twimg.com/profile_images/1208118895741001728/-t2DIJt-_normal.jpg",
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
