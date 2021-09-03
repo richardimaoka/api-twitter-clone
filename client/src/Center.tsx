7794fe3b8d898c371eb39a0435e85412bb215207
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-09-03 23:54:07 +0900

    barebone profiletop

--- client/src/Center.tsx
+++ client/src/Center.tsx
@@ -3,6 +3,10 @@ import { TweetList } from "./TweetList";
 import { AddTweetBox } from "./AddTweetBox";
 import { ProfileTop } from "./ProfileTop";
 
+interface CenterProps {
+  user: User;
+}
+
 export const Center = ({ user }: CenterProps) => (
   <div>
     <ProfileTop />
@@ -10,7 +14,3 @@ export const Center = ({ user }: CenterProps) => (
     <TweetList user={user} />
   </div>
 );
-
-interface CenterProps {
-  user: User;
-}
--- client/src/ProfileTop.tsx
+++ client/src/ProfileTop.tsx
@@ -1,1 +1,17 @@
-export const ProfileTop = () => <div></div>;
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+interface ProfileTopProps {}
+
+export const ProfileTop = ({}: ProfileTopProps) => {
+  return (
+    <div
+      css={css`
+        width: 600px;
+        border: 1px solid #cecece;
+      `}
+    >
+      <img src="" alt="" />
+    </div>
+  );
+};
